c1cc4139915c799fca6a4a3443266369f0ab17b7
Rodrigo Gomes <rodrigogsn@gmail.com>
2019-09-23 00:16:33 -0300

    Heroku Environment Variables

--- src/services/api.js
+++ src/services/api.js
@@ -3,10 +3,7 @@ import axios from 'axios';
 require('dotenv/config');
 
 const api = axios.create({
-  baseURL:
-    process.env.NODE_ENV === 'development'
-      ? 'http://localhost:5000'
-      : 'https://hackathon-organizer-back.herokuapp.com',
+  baseURL: process.env.REACT_APP_API_URL,
 });
 
 export default api;
