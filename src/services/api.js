0d0f2da6a01f5f1285347516ad25c9d85de99cf5
Rodrigo Gomes <rodrigogsn@gmail.com>
2019-09-26 09:11:14 -0300

    env fix and favicon

--- src/services/api.js
+++ src/services/api.js
@@ -1,10 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL:
-    process.env.NODE_ENV === 'development'
-      ? 'http://localhost:5000'
-      : process.env.REACT_APP_API_URL,
+  baseURL: process.env.REACT_APP_API_URL,
 });
 
 export default api;
